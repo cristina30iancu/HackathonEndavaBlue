3d91f1d9f61f1297a691dafe45488ecb3b2d0737
Elena Bianca Buduroes <biancaelena033@gmail.com>
2022-01-02 23:42:27 +0200

    Added ImageToText methods

---
+++ src/main/java/com/example/hackathonendava/ocr/ImageToText.java
@@ -0,0 +1,71 @@
+package com.example.hackathonendava.ocr;
+
+import org.bytedeco.javacpp.*;
+
+import static org.bytedeco.javacpp.lept.*;
+import static org.bytedeco.javacpp.tesseract.*;
+
+public class ImageToText {
+
+    static BytePointer resultText;
+    static PIX image;
+
+    static TessBaseAPI api = new TessBaseAPI();
+
+    public static BytePointer getResultText() {
+        return resultText;
+    }
+
+    public static void setResultText(BytePointer resultText) {
+        ImageToText.resultText = resultText;
+    }
+
+    public static PIX getImage() {
+        return image;
+    }
+
+    public static void setImage(PIX image) {
+        ImageToText.image = image;
+    }
+
+    public static void initializeTesseract() {
+
+        if (api.Init(".", "ENG") != 0) {
+            System.err.println("Could not initialize tesseract.");
+        }
+    }
+
+    public static void loadImage(String path) {
+        setImage(pixRead(path));
+        api.SetImage(image);
+    }
+
+    public static void calculateText() {
+        setResultText(api.GetUTF8Text());
+        String string = getResultText().getString();
+        System.out.println(string);
+    }
+
+    public static void cleanMemory() {
+        api.End();
+        resultText.deallocate();
+        pixDestroy(image);
+    }
+
+    // THIS METHOD SHOULD BE REFERENCED IN DIGITAL NOTES PAGE
+
+    public static void imageToText() {
+
+        initializeTesseract();
+        // loadImage should receive as argument the correct path
+        // this is just an example
+        loadImage("testImageToText.png");
+        calculateText();
+        cleanMemory();
+
+    }
+
+    public static void main(String[] args) {
+        imageToText();
+    }
+}
